d68a8b25702d7296a52f353fb242dc5a900ec3ee
elambrecht <elambrecht@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-24 20:04:31 +0000

    button styling. use blog title rather than description for station name.
    git-svn-id: https://plugins.svn.wordpress.org/feed-media-player/trunk@1013574 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- config-page.php
+++ config-page.php
@@ -9,12 +9,12 @@
 
     <script>
       var FeedMedia = {
-        iframe:        'https://developer.feed.fm/',
+        iframe:        'https://developer.feed.fm',
         defaultToken:  '<?php echo DEFAULT_FEED_TOKEN; ?>',
         defaultSecret: '<?php echo DEFAULT_FEED_SECRET; ?>',
         plugins:       '<?php echo admin_url("plugins.php"); ?>',
         home:          '<?php echo get_option('home'); ?>',
-        description:   '<?php echo get_option('blogdescription'); ?>',
+        description:   '<?php echo get_option('blogname'); ?>',
         options:       <?php echo json_encode(get_option('feed_media_player_options')); ?>
       };
     </script>
@@ -22,10 +22,8 @@
     <div id="feed-media-not-registered" class="hidden wrapper">
       <div class="content">
       <h3>Sign up with Feed.fm to customize your music player</h3>
-      <div class="clearfix">
-        <div class="button-wrapper">
-          <button class="button-primary" id="feed-media-sign-up">Sign-Up!</button>
-        </div>
+      <div class="button-holder">
+        <button class="button-primary" id="feed-media-sign-up">Sign-Up!</button>
       </div>
       
       </div>
@@ -34,16 +32,11 @@
     <div id="feed-media-registered" class="hidden wrapper">
       <div class="content">
         <h3>The Feed.fm Music Player is active on your site</h3>
-        <div class="clearfix">
-          <div class="button-wrapper">
+        <div class="button-holder">
           <button class="button-primary" id="feed-media-change-playlist">Change your playlist</button>
-          </div>
-          <div class="button-wrapper">
           <button class="button-primary" id="feed-media-change-skin">Change your player style</button>
-          </div>
-          <div id="feed-media-upsell" class="hidden button-wrapper">
+          <div id="feed-media-upsell" class="hidden">
             <button class="button-primary" id="feed-media-change-subscription">Upgrade to Premium!</button>
-          </div>
           </div>
         </div>
       </div>
